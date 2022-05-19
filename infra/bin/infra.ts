ecb43fd81dcc86a9bdf68b27360a50e7e0fb3331
Przemyslaw Michalek <pmichalek@apttension.com>
2022-05-19 20:02:51 +0200

    Load env config

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,20 +2,15 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { InfraStack } from '../lib/infra-stack';
+import { loadEnvSettings } from "../lib/settings";
 
-const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+(async () => {
+  const envSettings = await loadEnvSettings();
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  const getStackName = (baseName: string, prefix: string) =>
+    `${prefix}-${baseName}`;
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  const app = new cdk.App();
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+  // new EnvComponentsStack(app, getStackName("ComponentsStack", envSettings.projectEnvName), { envSettings });
+})
